ef36e8491e9204be282fdae45b74f3be2000c6c0
ikvi63270 <ikvi63270@edu.ucl.dk>
2024-09-13 09:09:58 +0200

    region update

--- Models/Region.cs
+++ Models/Region.cs
@@ -9,7 +9,7 @@ namespace RegionSyd.Models
     internal class Region
     {
         public int Id { get; set; }
-        public string Name { get; /set; }
+        public string Name { get; set; }
         public string System {  get; set; }
 
         public string GetRegionInfo()
